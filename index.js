344c49442a80ae8bb935c2fcf9dc5d9b1247baa3
Taya Simakova <stipaxa@gmail.com>
2023-10-24 19:56:27 +0100

    working on jwt

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-express = require("express")
-morgan = require("morgan")
-mongoose = require("mongoose")
+express = require('express')
+morgan = require('morgan')
+mongoose = require('mongoose')
+cors = require('cors')
+jwt = require('jsonwebtoken')
+jwkToPem = require('jwk-to-pem')
+axios = require('axios')
 require('dotenv').config()
-Note = require("./models/note")
+Note = require('./models/note')
 const app = express()
 const port = 3000
 
@@ -10,20 +14,39 @@ const port = 3000
 app.use(express.json())
 app.use(morgan('combined'))
 
-mongoose.connect(process.env.MONGO_URL).catch(function (err){
+let myIDkeys = null
+axios.get(process.env.MYID_URL).then((resp) => {
+    myIDkeys = resp.data
+})
+
+app.use(
+    cors({
+        origin: process.env.CORS_ALLOWED_ORIGINS
+            ? process.env.CORS_ALLOWED_ORIGINS.split(' ')
+            : '*',
+        credentials: true,
+    })
+)
+
+mongoose.connect(process.env.MONGO_URL).catch(function (err) {
     console.log(err)
 })
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log(`...Server started on port ${port}...`)
 })
 
-app.get("/ping", async function(req, res){
+app.get('/ping', async function (req, res) {
     res.status(200).send()
 })
 
+app.get('/test', async function (req, res) {
+    const u = getUserName(req)
+    res.status(200).send(u)
+})
+
 // Create note
-app.post("/notes", async function(req, res){ 
+app.post('/notes', async function (req, res) {
     try {
         const note = new Note({
             author: req.body.author,
@@ -41,7 +64,7 @@ app.post("/notes", async function(req, res){
 })
 
 // Read note
-app.get("/notes/:id", async function(req, res){
+app.get('/notes/:id', async function (req, res) {
     try {
         const note = await Note.findById(req.params.id)
         const result = {
@@ -51,7 +74,7 @@ app.get("/notes/:id", async function(req, res){
             updatedAt: note.updatedAt,
             title: note.title,
             text: note.text,
-            tags: note.tags,            
+            tags: note.tags,
         }
         res.json(result)
     } catch (e) {
@@ -61,9 +84,9 @@ app.get("/notes/:id", async function(req, res){
 })
 
 // Delete note
-app.delete("/notes/:id", async function(req, res){
+app.delete('/notes/:id', async function (req, res) {
     try {
-        await Note.deleteOne({_id: req.params.id})
+        await Note.deleteOne({ _id: req.params.id })
         res.status(200).send()
     } catch (e) {
         console.error(e)
@@ -72,9 +95,9 @@ app.delete("/notes/:id", async function(req, res){
 })
 
 // Update note
-app.put("/notes/:id", async function(req, res){
+app.put('/notes/:id', async function (req, res) {
     try {
-        await Note.findByIdAndUpdate({_id: req.params.id}, req.body)
+        await Note.findByIdAndUpdate({ _id: req.params.id }, req.body)
         res.status(200).send()
     } catch (e) {
         console.error(e)
@@ -83,10 +106,10 @@ app.put("/notes/:id", async function(req, res){
 })
 
 // List notes
-app.get("/notes", async function(req, res){
+app.get('/notes', async function (req, res) {
     try {
         const all_notes = await Note.find()
-        const result = all_notes.map(n => {
+        const result = all_notes.map((n) => {
             return {
                 id: n.id,
                 author: n.author,
@@ -95,7 +118,7 @@ app.get("/notes", async function(req, res){
                 title: n.title,
                 text: n.text,
                 tags: n.tags,
-            } 
+            }
         })
         res.status(200).json(result)
     } catch (e) {
@@ -103,3 +126,21 @@ app.get("/notes", async function(req, res){
         res.status(400).send()
     }
 })
+
+// return username from JWT or throw exception
+function getUserName(req) {
+    const token = req.get('Authorization').split(' ')[1]
+    const { username, token_use } = jwt.verify(
+        token,
+        jwkToPem(myIDkeys.keys[0]),
+        {
+            algirithms: ['RS256'],
+        }
+    )
+    if (token_use !== 'access') {
+        throw new Error('invalid token use')
+    }
+    //console.log(JSON.stringify(decoded_token, null, 2))
+
+    return username
+}
